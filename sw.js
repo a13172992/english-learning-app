bbc61db7375b26b955c8493ea973541da40f2614
a13172992 <152038739+a13172992@users.noreply.github.com>
2026-04-10 23:24:40 +0800

    Add PWA support for mobile app installation

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'english-learning-v1';
+const urlsToCache = [
+    '/english-learning-app/',
+    '/english-learning-app/index.html',
+    '/english-learning-app/manifest.json',
+    '/english-learning-app/icon-192.png',
+    '/english-learning-app/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
